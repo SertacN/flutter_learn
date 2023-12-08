335e47a93e7c2c2cc7cdd8972df3f33ea89e15b7
Sertaç Can <82063025+SertacN@users.noreply.github.com>
2023-12-08 18:38:11 +0300

    text component learn

---
+++ flutter_learn/lib/100/text_learn_view.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/material.dart';
+
+class TextLearnView extends StatelessWidget {
+  TextLearnView({
+    super.key,
+    this.userName,
+  });
+  final String myName = 'Sertaç';
+  final ProjectMessage keys = ProjectMessage();
+  final String? userName;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Center(
+              child: Text(
+            'Welcome $myName ${myName.length}',
+            style: const TextStyle(
+              color: Colors.lime,
+              fontSize: 16,
+              decoration: TextDecoration.underline,
+              letterSpacing: 2,
+              fontStyle: FontStyle.italic,
+            ),
+          )),
+          Text('Hello $myName ${myName.length}',
+              style: ProjectStyle.welcomeStyle),
+          Text('Hello $myName ${myName.length}',
+              style: Theme.of(context)
+                  .textTheme
+                  .headlineLarge
+                  ?.copyWith(color: Colors.blueAccent)),
+          Text(keys.welcome),
+          Text(userName ?? '')
+        ],
+      ),
+    );
+  }
+}
+
+class ProjectStyle {
+  static TextStyle get welcomeStyle => const TextStyle(
+      color: Colors.lime,
+      fontSize: 16,
+      decoration: TextDecoration.underline,
+      letterSpacing: 2,
+      fontStyle: FontStyle.italic);
+}
+
+class ProjectMessage {
+  String welcome = 'Hello';
+}
