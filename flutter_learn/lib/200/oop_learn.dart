86b6a950ccc59f7b27d80617fcded788b3b168fb
Sertaç Can <82063025+SertacN@users.noreply.github.com>
2024-01-02 20:00:16 +0300

    oop learn, implement-extends-mixin

--- flutter_learn/lib/200/oop_learn.dart
+++ flutter_learn/lib/200/oop_learn.dart
@@ -1,7 +1,10 @@
 import 'dart:io';
 
+// implement: üst sınıfta ne varsa hepsini kullanmak zorunda. boş dolu farketmez
+// extends:   üst sınıfta içi boş olanları kullanmasakta olur. void toShare() {}
 abstract class IFileDowload {
   bool? downloadItem(FileItem? fileItem);
+  void toShare() {}
 }
 
 class FileDownload implements IFileDowload {
@@ -11,12 +14,14 @@ class FileDownload implements IFileDowload {
 
     return true;
   }
+
+  @override
+  void toShare() {}
 }
 
 class SertacDowload extends IFileDowload with ShareMixin {
   @override
   bool? downloadItem(FileItem? fileItem) {
-    // TODO: implement downloadItem
     throw UnimplementedError();
   }
 }
